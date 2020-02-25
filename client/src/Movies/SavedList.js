5b33e01f4371f0cfd0d7e5ad19f4b1c0449172ac
M Groesbeck <m.groesbeck@gmail.com>
2020-02-25 15:24:43 -0800

    Adjusted function calls.

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-const SavedList = props => (
-  <div className="saved-list">
-    <h3>Saved Movies:</h3>
-    {props.list.map(movie => (
-      <span className="saved-movie"><NavLink to={`/movies/${movie.id}`}>{movie.title}</NavLink></span>
-    ))}
-    <div className="home-button"><NavLink to="/">Home</NavLink></div>
-  </div>
-);
+function SavedList(props) {
+  console.log(props);
+  return (
+    <div className="saved-list">
+      <h3>Saved Movies:</h3>
+      {props.list.map(movie => (
+        <span className="saved-movie">
+          <NavLink to={`/movies/${movie.id}`}>{movie.title}</NavLink>
+        </span>
+      ))}
+      <div className="home-button"><NavLink to="/">Home</NavLink>
+      </div>
+    </div>
+  );
+}
 
 export default SavedList;
